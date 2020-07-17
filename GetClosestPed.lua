5f728ead9d594364137a89a42138c3a4cbfeceae
MoneyWasted <45946092+MoneyWasted@users.noreply.github.com>
2020-07-17 11:22:17 -0400

    Create GetClosestPed.lua

---
+++ GetClosestPed.lua
@@ -0,0 +1,15 @@
+function GetClosestPed()
+    local closestPed = 0
+
+    for ped in EnumeratePeds() do
+        local distanceCheck = GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), GetEntityCoords(ped), true)
+        if distanceCheck <= 1.5 and ped ~= GetPlayerPed(-1) and not IsPedAPlayer(ped)  then
+            closestPed = ped
+            break
+
+		elseif ped == GetPlayerPed(-1) then
+        end
+    end
+
+    return closestPed
+end
