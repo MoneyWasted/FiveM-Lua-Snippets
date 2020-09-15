651ba6e4b5ee9793ae1c97f239bbb78b522ef3fe
MoneyWasted <45946092+MoneyWasted@users.noreply.github.com>
2020-09-15 02:24:06 +0000

    Create GetCamDirection.lua

---
+++ GetCamDirection.lua
@@ -0,0 +1,17 @@
+local function GetCamDirection()
+    local heading = GetGameplayCamRelativeHeading() + GetEntityHeading(PlayerPedId())
+    local pitch = GetGameplayCamRelativePitch()
+    
+    local x = -math.sin(heading * math.pi / 180.0)
+    local y = math.cos(heading * math.pi / 180.0)
+    local z = math.sin(pitch * math.pi / 180.0)
+    
+    local len = math.sqrt(x * x + y * y + z * z)
+    if len ~= 0 then
+        x = x / len
+        y = y / len
+        z = z / len
+    end
+    
+    return x, y, z
+end
