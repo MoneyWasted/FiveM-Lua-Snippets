9989f512f3051d860fcaf1421262a2efdc413757
MoneyWasted <45946092+MoneyWasted@users.noreply.github.com>
2020-09-15 02:22:30 +0000

    Create WorldToScreen.lua

---
+++ WorldToScreen.lua
@@ -0,0 +1,10 @@
+function WorldToScreen(worldCoords)
+    local check, x, y = GetScreenCoordFromWorldCoord(worldCoords.x, worldCoords.y, worldCoords.z)
+    if not check then
+        return false
+    end
+    
+    screenCoordsx = (x - 0.5) * 2.0
+    screenCoordsy = (y - 0.5) * 2.0
+    return true, screenCoordsx, screenCoordsy
+end
