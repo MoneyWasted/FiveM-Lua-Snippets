4a104f7e068257d21c1b3fdfedcfcb27005a54d5
MoneyWasted <45946092+MoneyWasted@users.noreply.github.com>
2020-09-15 02:19:46 +0000

    Create ScreenToWorld.lua

---
+++ ScreenToWorld.lua
@@ -0,0 +1,43 @@
+function ScreenToWorld(screenCoord)
+    local camRot = GetGameplayCamRot(2)
+    local camPos = GetGameplayCamCoord()
+    
+    local vect2x = 0.0
+    local vect2y = 0.0
+    local vect21y = 0.0
+    local vect21x = 0.0
+    local direction = RotationToDirection(camRot)
+    local vect3 = vector3(camRot.x + 10.0, camRot.y + 0.0, camRot.z + 0.0)
+    local vect31 = vector3(camRot.x - 10.0, camRot.y + 0.0, camRot.z + 0.0)
+    local vect32 = vector3(camRot.x, camRot.y + 0.0, camRot.z + -10.0)
+    
+    local direction1 = RotationToDirection(vector3(camRot.x, camRot.y + 0.0, camRot.z + 10.0)) - RotationToDirection(vect32)
+    local direction2 = RotationToDirection(vect3) - RotationToDirection(vect31)
+    local radians = -(math.rad(camRot.y))
+    
+    vect33 = (direction1 * math.cos(radians)) - (direction2 * math.sin(radians))
+    vect34 = (direction1 * math.sin(radians)) - (direction2 * math.cos(radians))
+    
+    local case1, x1, y1 = WorldToScreenRel(((camPos + (direction * 10.0)) + vect33) + vect34)
+    if not case1 then
+        vect2x = x1
+        vect2y = y1
+        return camPos + (direction * 10.0)
+    end
+    
+    local case2, x2, y2 = WorldToScreenRel(camPos + (direction * 10.0))
+    if not case2 then
+        vect21x = x2
+        vect21y = y2
+        return camPos + (direction * 10.0)
+    end
+    
+    if math.abs(vect2x - vect21x) < 0.001 or math.abs(vect2y - vect21y) < 0.001 then
+        return camPos + (direction * 10.0)
+    end
+    
+    local x = (screenCoord.x - vect21x) / (vect2x - vect21x)
+    local y = (screenCoord.y - vect21y) / (vect2y - vect21y)
+    return ((camPos + (direction * 10.0)) + (vect33 * x)) + (vect34 * y)
+
+end
