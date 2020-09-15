4e9bb013f22be9676a10fa8bc6c3a91e195ce3bb
MoneyWasted <45946092+MoneyWasted@users.noreply.github.com>
2020-09-15 02:25:09 +0000

    Create Oscillate.lua

---
+++ Oscillate.lua
@@ -0,0 +1,7 @@
+function Oscillate(entity, position, angleFreq, dampRatio)
+    local pos1 = ScaleVector(SubVectors(position, GetEntityCoords(entity)), (angleFreq * angleFreq))
+    local pos2 = AddVectors(ScaleVector(GetEntityVelocity(entity), (2.0 * angleFreq * dampRatio)), vector3(0.0, 0.0, 0.1))
+    local targetPos = SubVectors(pos1, pos2)
+    
+    ApplyForce(entity, targetPos)
+end
