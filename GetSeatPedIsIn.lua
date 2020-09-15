957f5c056e2531ab3ec344332aed4e6a83e0c2e8
MoneyWasted <45946092+MoneyWasted@users.noreply.github.com>
2020-09-15 02:17:54 +0000

    Create GetSeatPedIsIn.lua

---
+++ GetSeatPedIsIn.lua
@@ -0,0 +1,9 @@
+function GetSeatPedIsIn(ped)
+    if not IsPedInAnyVehicle(ped, false) then return
+    else
+        veh = GetVehiclePedIsIn(ped)
+        for i = 0, GetVehicleMaxNumberOfPassengers(veh) do
+            if GetPedInVehicleSeat(veh) then return i end
+        end
+    end
+end
