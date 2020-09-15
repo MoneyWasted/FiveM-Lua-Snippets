40513c3617f33c67eea0288d88b7f3a708db3d38
MoneyWasted <45946092+MoneyWasted@users.noreply.github.com>
2020-09-15 02:23:31 +0000

    Create RotationToDirection.lua

---
+++ RotationToDirection.lua
@@ -0,0 +1,6 @@
+function RotationToDirection(rotation)
+    local retz = math.rad(rotation.z)
+    local retx = math.rad(rotation.x)
+    local absx = math.abs(math.cos(retx))
+    return vector3(-math.sin(retz) * absx, math.cos(retz) * absx, math.sin(retx))
+end
