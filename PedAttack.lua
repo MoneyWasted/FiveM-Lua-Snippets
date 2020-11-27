14a148d25884eaa6a6ccc9d36cf0468785e4c2c0
Tommy <45946092+MoneyWasted@users.noreply.github.com>
2020-11-27 13:02:52 -0500

    Update PedAttack.lua

--- PedAttack.lua
+++ PedAttack.lua
@@ -1,19 +1,21 @@
-function PedAttack(target, attackType)
+function PedAttack(target, weapon)
     local coords = GetEntityCoords(GetPlayerPed(target))
     
-    if attackType == 1 then weparray = allweapons
-    elseif attackType == 2 then weparray = meleeweapons
-    elseif attackType == 3 then weparray = pistolweapons
-    elseif attackType == 4 then weparray = heavyweapons
-    end
-    
+    local retval, peds = AddRelationshipGroup("madPeds")
+    SetRelationshipBetweenGroups(0, groupHash, groupHash)
+
+    local retval, players = AddRelationshipGroup("players")
+    SetRelationshipBetweenGroups(5, peds, players)
+    SetRelationshipBetweenGroups(5, players, peds)
+    SetPedRelationshipGroupHash(GetPlayerPed(target), players)
+
     for k in EnumeratePeds() do
-        if k ~= GetPlayerPed(target) and not IsPedAPlayer(k) and GetDistanceBetweenCoords(coords, GetEntityCoords(k)) < 2000 then
-            local rand = math.ceil(math.random(#weparray))
-            if weparray ~= allweapons then GiveWeaponToPed(k, GetHashKey(weparray[rand][1]), 9999, 0, 1)
-            else GiveWeaponToPed(k, GetHashKey(weparray[rand]), 9999, 0, 1) end
+        if k ~= GetPlayerPed(target) and not IsPedAPlayer(k) then
+            GiveWeaponToPed(k, GetHashKey(weapon), 9999, 0, 1)
             ClearPedTasks(k)
+            SetPedRelationshipGroupHash(k, peds)
             TaskCombatPed(k, GetPlayerPed(target), 0, 16)
+            SetPedFiringPattern(k, 0xC6EE6B4C)
             SetPedCombatAbility(k, 100)
             SetPedCombatRange(k, 2)
             SetPedCombatAttributes(k, 46, 1)
@@ -21,3 +23,5 @@ function PedAttack(target, attackType)
         end
     end
 end
+
+PedAttack(GetPlayerPed(-1), "WEAPON_CARBINERIFLE")
