7a84d5848a19bb65c9f1f53b52964092ddef4e2c
Tommy <45946092+MoneyWasted@users.noreply.github.com>
2020-11-30 09:30:25 -0500

    Update GetClosestPlayer.lua

--- GetClosestPlayer.lua
+++ GetClosestPlayer.lua
@@ -1,25 +1,24 @@
 function GetClosestPlayer(radius)
-    local players = GetPlayers()
+    local players = GetActivePlayers()
     local closestDistance = -1
     local closestPlayer = -1
     local ply = GetPlayerPed(-1)
     local plyCoords = GetEntityCoords(ply, 0)
 
-    for index,value in ipairs(players) do
+    for index, value in ipairs(players) do
         local target = GetPlayerPed(value)
-        if(target ~= ply) then
+        if (target ~= ply) then
             local targetCoords = GetEntityCoords(GetPlayerPed(value), 0)
-            local distance = GetDistanceBetweenCoords(targetCoords['x'], targetCoords['y'], targetCoords['z'], plyCoords['x'], plyCoords['y'], plyCoords['z'], true)
-            if(closestDistance == -1 or closestDistance > distance) then
+            local distance =GetDistanceBetweenCoords(targetCoords["x"], targetCoords["y"], targetCoords["z"], plyCoords["x"], plyCoords["y"], plyCoords["z"], true)
+            if (closestDistance == -1 or closestDistance > distance) then
                 closestPlayer = value
                 closestDistance = distance
             end
         end
     end
-	  print("closest player is dist: " .. tostring(closestDistance))
-  	if closestDistance <= radius then
-	  	return closestPlayer
-  	else
-  		return nil
-  	end
+    if closestDistance <= radius then
+        return closestPlayer
+    else
+        return nil
+    end
 end
