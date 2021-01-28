ef50007d2a5e62c7e4975cb830a52920eab0d7b3
Tommy <45946092+MoneyWasted@users.noreply.github.com>
2021-01-28 18:14:43 -0500

    Create RGBRainbow.lua

---
+++ RGBRainbow.lua
@@ -0,0 +1,10 @@
+function RGBRainbow(frequency)
+    local result = {}
+    local curtime = GetGameTimer() / 1000
+
+    result.r = math.floor(math.sin(curtime * frequency + 0) * 127 + 128)
+    result.g = math.floor(math.sin(curtime * frequency + 2) * 127 + 128)
+    result.b = math.floor(math.sin(curtime * frequency + 4) * 127 + 128)
+
+    return result
+end
