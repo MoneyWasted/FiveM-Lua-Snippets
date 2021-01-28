4b8f485dd79a98016d4d0131fcfa0f421437f6b2
Tommy <45946092+MoneyWasted@users.noreply.github.com>
2021-01-28 18:28:50 -0500

    Update CreateFireworks.lua

--- CreateFireworks.lua
+++ CreateFireworks.lua
@@ -1,38 +1,18 @@
 Citizen.CreateThread(function()
+    local pedcoords = GetEntityCoords(GetPlayerPed(-1))
+    local times = 20
 
-	RequestAnimDict(animlib)
-
-	while not HasAnimDictLoaded(animlib) do
-		   Citizen.Wait(10)
+    if not HasNamedPtfxAssetLoaded("scr_indep_fireworks") then
+        RequestNamedPtfxAsset("scr_indep_fireworks")
+        while not HasNamedPtfxAssetLoaded("scr_indep_fireworks") do
+            Wait(10)
+        end
     end
-        
-	if not HasNamedPtfxAssetLoaded("scr_indep_fireworks") then
-		RequestNamedPtfxAsset("scr_indep_fireworks")
-		while not HasNamedPtfxAssetLoaded("scr_indep_fireworks") do
-		   Wait(10)
-		end
-	end
 
-	local pedcoords = GetEntityCoords(GetPlayerPed(-1))
-	local ped = GetPlayerPed(-1)
-	local times = 20
-
-	TaskPlayAnim(ped, animlib, 'place_firework_3_box', -1, -8.0, 3000, 0, 0, false, false, false)
-	Citizen.Wait(4000)
-	ClearPedTasks(ped)
-		   
-	box = CreateObject(GetHashKey('ind_prop_firework_03'), pedcoords, true, false, false)
-	PlaceObjectOnGroundProperly(box)
-	FreezeEntityPosition(box, true)
-	local firecoords = GetEntityCoords(box)
-
-	Citizen.Wait(10000)
-	repeat
-	UseParticleFxAssetNextCall("scr_indep_fireworks")
-	local part1 = StartNetworkedParticleFxNonLoopedAtCoord("scr_indep_firework_trailburst", firecoords, 0.0, 0.0, 0.0, 1.0, false, false, false, false)
-	times = times - 1
-	Citizen.Wait(2000)
-	until(times == 0)
-	DeleteEntity(box)
-	box = nil
+    repeat
+        UseParticleFxAssetNextCall("scr_indep_fireworks")
+        local firework = StartNetworkedParticleFxNonLoopedAtCoord("scr_indep_firework_trailburst", pedcoords, 0.0, 0.0, 0.0, 1.0, false, false, false, false)
+        times = times - 1
+        Citizen.Wait(2000)
+    until (times == 0)
 end)
