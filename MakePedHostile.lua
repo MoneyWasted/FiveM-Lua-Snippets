ed678a76cb078c898fbbb60d4f56b0919dae34f1
Tommy <45946092+MoneyWasted@users.noreply.github.com>
2021-01-28 18:23:02 -0500

    Update MakePedHostile.lua

--- MakePedHostile.lua
+++ MakePedHostile.lua
@@ -1,29 +1,27 @@
-function makePedHostile(target, ped, swat, clone) -- RipTide Mod Menu Remade Function
+function makePedHostile(target, ped, swat, clone)
     if swat == 1 or swat == true then
         RequestNetworkControl(ped)
         TaskCombatPed(ped, GetPlayerPed(SelectedPlayer), 0, 16)
         SetPedCanSwitchWeapon(ped, true)
-    else
-        if clone == 1 or clone == true then
-            local Hash = GetEntityModel(ped)
-            if DoesEntityExist(ped) then
-                DeletePed(ped)
-                RequestModel(Hash)
-                local coords = GetEntityCoords(GetPlayerPed(target), true)
-                if HasModelLoaded(Hash) then
-                    local newPed = CreatePed(21, Hash, coords.x, coords.y, coords.z, 0, 1, 0)
-                    if GetEntityHealth(newPed) == GetEntityMaxHealth(newPed) then
-                        SetModelAsNoLongerNeeded(Hash)
-                        RequestNetworkControl(newPed)
-                        TaskCombatPed(newPed, GetPlayerPed(target), 0, 16)
-                        SetPedCanSwitchWeapon(ped, true)
-                    end
+    elseif clone == 1 or clone == true then
+        local Hash = GetEntityModel(ped)
+        if DoesEntityExist(ped) then
+            DeletePed(ped)
+            RequestModel(Hash)
+            local coords = GetEntityCoords(GetPlayerPed(target), true)
+            if HasModelLoaded(Hash) then
+                local newPed = CreatePed(21, Hash, coords.x, coords.y, coords.z, 0, 1, 0)
+                if GetEntityHealth(newPed) == GetEntityMaxHealth(newPed) then
+                    SetModelAsNoLongerNeeded(Hash)
+                    RequestNetworkControl(newPed)
+                    TaskCombatPed(newPed, GetPlayerPed(target), 0, 16)
+                    SetPedCanSwitchWeapon(ped, true)
                 end
             end
-        else
-            local TargetHandle = GetPlayerPed(target)
-            RequestNetworkControl(ped)
-            TaskCombatPed(ped, TargetHandle, 0, 16)
         end
+    else
+        local TargetHandle = GetPlayerPed(target)
+        RequestNetworkControl(ped)
+        TaskCombatPed(ped, TargetHandle, 0, 16)
     end
 end
