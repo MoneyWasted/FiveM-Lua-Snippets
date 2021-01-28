58e7490b05d958dea32bf60ab7004c52196d6ae0
Tommy <45946092+MoneyWasted@users.noreply.github.com>
2021-01-28 18:18:48 -0500

    Update TeleportToWaypoint.lua

--- TeleportToWaypoint.lua
+++ TeleportToWaypoint.lua
@@ -1,44 +1,40 @@
 function TeleportToWaypoint()
-	if DoesBlipExist(GetFirstBlipInfoId(8)) then
-		local blipIterator = GetBlipInfoIdIterator(8)
-		local blip = GetFirstBlipInfoId(8, blipIterator)
-		WaypointCoords = Citizen.InvokeNative(0xFA7C7F0AADF25D09, blip, Citizen.ResultAsVector()) --Thanks To Briglair [forum.FiveM.net]
-		wp = true
-	else
-		drawNotification("~r~No waypoint!")
-	end
+    if DoesBlipExist(GetFirstBlipInfoId(8)) then
+        local blipIterator = GetBlipInfoIdIterator(8)
+        local blip = GetFirstBlipInfoId(8, blipIterator)
+        WaypointCoords = Citizen.InvokeNative(0xFA7C7F0AADF25D09, blip, Citizen.ResultAsVector()) --Thanks To Briglair [forum.FiveM.net]
+        wp = true
+    else
+	break
+    end
 
-	local zHeigt = 0.0
-	height = 1000.0
-	while true do
-		Citizen.Wait(0)
-		if wp then
-			if
-				IsPedInAnyVehicle(GetPlayerPed(-1), 0) and
-					(GetPedInVehicleSeat(GetVehiclePedIsIn(GetPlayerPed(-1), 0), -1) == GetPlayerPed(-1))
-			 then
-				entity = GetVehiclePedIsIn(GetPlayerPed(-1), 0)
-			else
-				entity = GetPlayerPed(-1)
-			end
+    local zHeigt = 0.0
+    height = 1000.0
+    while true do
+        Citizen.Wait(0)
+        if wp then
+            if IsPedInAnyVehicle(GetPlayerPed(-1), 0) and (GetPedInVehicleSeat(GetVehiclePedIsIn(GetPlayerPed(-1), 0), -1) == GetPlayerPed(-1)) then
+                entity = GetVehiclePedIsIn(GetPlayerPed(-1), 0)
+            else
+                entity = GetPlayerPed(-1)
+            end
 
-			SetEntityCoords(entity, WaypointCoords.x, WaypointCoords.y, height)
-			FreezeEntityPosition(entity, true)
-			local Pos = GetEntityCoords(entity, true)
+            SetEntityCoords(entity, WaypointCoords.x, WaypointCoords.y, height)
+            FreezeEntityPosition(entity, true)
+            local Pos = GetEntityCoords(entity, true)
 
-			if zHeigt == 0.0 then
-				height = height - 25.0
-				SetEntityCoords(entity, Pos.x, Pos.y, height)
-				bool, zHeigt = GetGroundZFor_3dCoord(Pos.x, Pos.y, Pos.z, 0)
-			else
-				SetEntityCoords(entity, Pos.x, Pos.y, zHeigt)
-				FreezeEntityPosition(entity, false)
-				wp = false
-				height = 1000.0
-				zHeigt = 0.0
-				drawNotification("~g~Teleported to waypoint!")
-				break
-			end
-		end
-	end
+            if zHeigt == 0.0 then
+                height = height - 25.0
+                SetEntityCoords(entity, Pos.x, Pos.y, height)
+                bool, zHeigt = GetGroundZFor_3dCoord(Pos.x, Pos.y, Pos.z, 0)
+            else
+                SetEntityCoords(entity, Pos.x, Pos.y, zHeigt)
+                FreezeEntityPosition(entity, false)
+                wp = false
+                height = 1000.0
+                zHeigt = 0.0
+                break
+            end
+        end
+    end
 end
