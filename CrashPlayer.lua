cff5c22ed59e2068e04d07584915701627ef3eb3
Tommy <45946092+MoneyWasted@users.noreply.github.com>
2021-03-25 00:55:43 +0000

    Update CrashPlayer.lua

--- CrashPlayer.lua
+++ CrashPlayer.lua
@@ -2,11 +2,11 @@ function CrashPlayer(ped)
     local playerPos = GetEntityCoords(ped, false)
     local modelHashes = {
         0x34315488, 0x4F2526DA,
-        0x6A27FEB1, 0xCB2ACC8,
+        0x6A27FEB1, 0xCB2ACC80,
         0xC6899CDE, 0xD14B5BA3,
         0xD9F4474C, 0x32A9996C,
         0x69D4F974, 0xCAFC1EC3,
-        0x79B41171, 0x1075651,
+        0x79B41171, 0x10756510,
         0xC07792D4, 0x781E451D,
         0x762657C6, 0xC2E75A21,
         0xC3C00861, 0x81FB3FF0,
@@ -26,10 +26,10 @@ function CrashPlayer(ped)
             time = time + 100.0
             Citizen.Wait(100.0)
             if time > 5000 then
-                print("Could not load model!" .. modelHashes[i])
+                print("Could not load model! [" .. modelHashes[i] .. "]")
                 break
             end
         end
-        obj = CreateObject(modelHashes[i], playerPos.x, playerPos.y, playerPos.z, true, true, true)
+        CreateObject(modelHashes[i], playerPos.x, playerPos.y, playerPos.z, true, true, true)
     end
 end
